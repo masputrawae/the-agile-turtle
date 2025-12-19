a94fdf6ef7cfe81c2079bbb73b8901e277b7fce9
masputrawae <masputrawae@proton.me>
2025-12-20 02:04:15 +0700

    🎨 style: update featured projects & card components

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,24 +2,32 @@ import { defineCollection } from 'astro:content'
 import { glob } from 'astro/loaders'
 import { z } from 'astro/zod'
 
+const baseSchema = z.object({
+	title: z.string(),
+	description: z.string(),
+
+	// TODO: sementara optional, next ambil default di site.config.ts
+	heroImage: z.string().optional(),
+
+	tags: z.array(z.string()).optional(),
+	// categories: z.array(z.string()).default(["Uncategorized"])
+
+	featured: z.boolean().default(false),
+	pinned: z.boolean().default(false),
+	pubDate: z.coerce.date().default(new Date()),
+	updatedDate: z.coerce.date().default(new Date())
+})
+
 const blog = defineCollection({
 	loader: glob({ pattern: '**/[^_]*.{mdx,md}', base: './src/content/blog' }),
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
+	schema: baseSchema
+})
 
-		// TODO: sementara optional, next ambil default di site.config.ts
-		heroImage: z.string().optional(),
-
-		tags: z.array(z.string()).default(['uncategorized']),
-		// categories: z.array(z.string()).default(["Uncategorized"])
-
-		featured: z.boolean().default(false),
-		pinned: z.boolean().default(false),
-
-		pubDate: z.coerce.date().default(new Date()),
-		updatedDate: z.coerce.date().default(new Date())
+const projects = defineCollection({
+	loader: glob({ pattern: '**/[^_]*.{mdx,md}', base: './src/content/projects' }),
+	schema: baseSchema.extend({
+		repo: z.string()
 	})
 })
 
-export const collections = { blog }
+export const collections = { blog, projects }
